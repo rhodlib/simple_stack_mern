9e301a30a5f29d04adf2968c77e6a8c6670f8221
rhodlib <rhodlib@gmail.com>
2020-04-14 16:06:05 -0300

    database connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 //Imports
+require("dotenv").config();
 const app = require("./app");
+require("./db/mongoose");
 
+//Server functions
 async function main () {
     await app.listen(4000);
     console.log("Server on port 4000");
