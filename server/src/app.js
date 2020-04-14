4d08a3b3d404ac2bdabc946c27bf35b5e36cf2f1
rhodlib <rhodlib@gmail.com>
2020-04-14 15:43:10 -0300

    first commit server structure

---
+++ server/src/app.js
@@ -0,0 +1,8 @@
+//Imports
+const express = require("express");
+
+//App
+const app = express();
+
+//Export
+module.exports = app;
---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+//Imports
+const app = require("./app");
+
+async function main () {
+    await app.listen(4000);
+    console.log("Server on port 4000");
+}
+
+//Server listen
+main();
