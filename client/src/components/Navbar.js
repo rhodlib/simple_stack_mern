5c7be374653fb7832adb7a7563f4d386f04b5baf
rhodlib <rhodlib@gmail.com>
2020-04-15 11:53:25 -0300

    bootstrap added and routing

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,47 @@
 //Imports
 import React from "react";
+import { Link } from "react-router-dom";
 
 //Navbar component
-export const Navbar = () => <div>Navbar</div>;
+export const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <div className="container">
+      <Link className="navbar-brand" to="/">
+        Notes App
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">
+              Notes
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/create">
+              Create Note
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/user">
+              Create User
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
 
 //Export
 export default Navbar;
